f22f63af12cf98def5fcffc250a18cd4ab271ae4
idcomcn <idcomcn2003@163.com>
2024-01-28 11:17:38 +0800

    init app

---
+++ 2024-february/README.md
@@ -0,0 +1,6 @@
+# android-month-2024
+
+## 开发工具-android-studio-v3.5
+
+这是一个android 2024年的每月一例-2月的例子
+获取手机线程，内存等信息
---
+++ 2024-february/app/src/main/java/com/czh/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.czh.myapplication;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+    }
+
+
+    private void bindViews() {
+        textView = findViewById(R.id.text_view);
+    }
+
+    public void onClickGet(View view) {
+        Log.e("idcomcn", "onClick: 点击了确定");
+        final OkHttpClient okHttpClient = new OkHttpClient()
+                .newBuilder()
+                .build();
+        final Request request = new Request.Builder()
+                .url("https://www.publicobject.com/helloworld.txt")
+                .header("User-Agent", "OkHttp Example")
+                .build();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Response response = okHttpClient.newCall(request).execute();
+                    String string = response.body().string();
+                    Log.d("idcomcn", "response=====" + string);
+                    //idcomcn TODO
+                    textView.setText("下载完成。");
+                    response.body().close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+}
